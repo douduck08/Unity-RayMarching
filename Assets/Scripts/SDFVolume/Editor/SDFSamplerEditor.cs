f19301debe93dbaeacba7908e9ae00af554e69fb
douduck08 <lu10082000@gmail.com>
2019-06-08 22:21:03 +0800

    update GPU sdf volume baker

---
+++ Assets/Scripts/SDFVolume/Editor/SDFSamplerEditor.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CanEditMultipleObjects]
+[CustomEditor (typeof (SDFSampler))]
+public class SDFSamplerEditor : Editor {
+
+    SerializedProperty resolutionProperty;
+    SerializedProperty scaleProperty;
+    SerializedProperty iterationProperty;
+    SerializedProperty useGpuProperty;
+
+    void OnEnable () {
+        resolutionProperty = serializedObject.FindProperty ("resolution");
+        scaleProperty = serializedObject.FindProperty ("scale");
+        iterationProperty = serializedObject.FindProperty ("iteration");
+        useGpuProperty = serializedObject.FindProperty ("useGpu");
+    }
+
+    public override void OnInspectorGUI () {
+        serializedObject.Update ();
+
+        EditorGUILayout.PropertyField (resolutionProperty);
+        EditorGUILayout.PropertyField (scaleProperty);
+        EditorGUILayout.PropertyField (iterationProperty);
+        EditorGUILayout.PropertyField (useGpuProperty);
+
+        if (!useGpuProperty.boolValue) {
+            EditorGUILayout.HelpBox ("CPU sampling is unsigned distance field now", MessageType.Info);
+        }
+
+        serializedObject.ApplyModifiedProperties ();
+
+        if (GUILayout.Button ("Create Volume Data")) CreateVolumeData ();
+    }
+
+    void CreateVolumeData () {
+        string path = EditorUtility.SaveFilePanelInProject ("Save SDF Volume into SDF asset", "", "asset", "");
+        if (string.IsNullOrEmpty (path)) {
+            return;
+        }
+
+        var output = new List<Object> ();
+        foreach (SDFSampler sampler in targets) {
+            var newPath = AssetDatabase.GenerateUniqueAssetPath (path);
+            var asset = ScriptableObject.CreateInstance<SDFAsset> ();
+            asset.Initialize (sampler);
+
+            AssetDatabase.CreateAsset (asset, newPath);
+            AssetDatabase.AddObjectToAsset (asset.texture, asset);
+            output.Add (asset);
+        }
+
+        AssetDatabase.SaveAssets ();
+        EditorUtility.FocusProjectWindow ();
+        Selection.objects = output.ToArray ();
+    }
+}
---
+++ Assets/Scripts/SDFVolume/SDFVolume.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class SDFVolume : MonoBehaviour {
+
+    [SerializeField] SDFAsset sdfAsset;
+    [SerializeField] Color color = Color.white;
+    [SerializeField, Range (0f, 1f)] float glossiness = 0.5f;
+    [SerializeField, Range (0f, 1f)] float metallic = 0f;
+
+    MeshRenderer meshRenderer;
+    Material material;
+
+    void OnEnable () {
+        material = new Material (Shader.Find ("Custom/SDFVolume"));
+        material.name = "Custom/SDFVolume";
+
+        meshRenderer = GetComponent<MeshRenderer> ();
+        meshRenderer.sharedMaterial = material;
+
+        OnValidate ();
+    }
+
+    void OnValidate () {
+        if (material == null || sdfAsset == null) return;
+
+        material.SetTexture ("_Volume", sdfAsset.texture);
+        material.SetColor ("_Color", color);
+        material.SetFloat ("_Glossiness", glossiness);
+        material.SetFloat ("_Metallic", metallic);
+    }
+}
